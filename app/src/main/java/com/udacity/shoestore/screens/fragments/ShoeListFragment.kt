8f12ebf7aa7804045cd1dae1703f1a20d9a22faf
tariqywsf <tarek.buhdeima@gmail.com>
2021-10-23 23:25:15 +0300

    fix ShoeStoreViewModel.kt fix ShoeListFragment.kt make item_shoe_list.xml add empty view to linear layot in fragment_shoe_list.xml

--- app/src/main/java/com/udacity/shoestore/screens/fragments/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/fragments/ShoeListFragment.kt
@@ -1,17 +1,27 @@
 package com.udacity.shoestore.screens.fragments
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.*
+import android.view.View.GONE
+import androidx.constraintlayout.widget.ConstraintSet.GONE
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.databinding.ItemShoeListBinding
+import com.udacity.shoestore.screens.viewmodels.ShoeStoreViewModel
 
-class ShoeListFragment: Fragment() {
+class ShoeListFragment : Fragment() {
+
     private lateinit var binding: FragmentShoeListBinding
+
+    private val shoeViewModel: ShoeStoreViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,11 +30,36 @@ class ShoeListFragment: Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
         setHasOptionsMenu(true)
 
+        binding.emptyView.visibility = View.VISIBLE
+
         binding.fab.setOnClickListener(
             Navigation.createNavigateOnClickListener(ShoeListFragmentDirections.actionShoeListFragmentToDetailFragment())
         )
 
+
+        displayShoes(inflater, container)
+
         return binding.root
+    }
+
+    private fun displayShoes(inflater: LayoutInflater, container: ViewGroup?) {
+
+        shoeViewModel.shoesList.observe(viewLifecycleOwner, {
+            it.forEach {
+
+                binding.emptyView.visibility = View.GONE
+                val shoeListItem =
+                    ItemShoeListBinding.inflate(LayoutInflater.from(requireContext()), null, false)
+                shoeListItem.apply {
+                    this.shoeName.text = getString(R.string.shoe_name, "Nike Air")
+                    this.company.text = getString(R.string.company, "Nike")
+                    this.shoeSize.text = getString(R.string.size, 42.0)
+                    this.description.text = getString(R.string.description, "Description about the shoe")
+                    this.images.text = getString(R.string.images, "some urls")
+                }
+                    binding.linearlayoutShoelist.addView(shoeListItem.root)
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -35,8 +70,10 @@ class ShoeListFragment: Fragment() {
     // Returns true if NavigationUI.onNavDestinationSelected return true, else
     // return super.onOptionsItemSelected.
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return NavigationUI.onNavDestinationSelected(item,
-            requireView().findNavController())
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            requireView().findNavController()
+        )
                 || super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/udacity/shoestore/screens/viewmodels/ShoeStoreViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/screens/viewmodels/ShoeStoreViewModel.kt
@@ -1,6 +1,5 @@
 package com.udacity.shoestore.screens.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,14 +8,14 @@ import timber.log.Timber
 
 class ShoeStoreViewModel : ViewModel() {
 
-    private val _shoesList = MutableLiveData<MutableList<Shoe>>(mutableListOf())
+    private val _shoesList = MutableLiveData<MutableList<Shoe>>()
     val shoesList: LiveData<MutableList<Shoe>>
         get() = _shoesList
 
-
-
     fun addShoe(shoe: Shoe) {
-       _shoesList.value?.add(shoe)
+
+        _shoesList.value?.add(shoe)
         Timber.i("addShoe() called with: shoe = $shoe")
+
     }
 }
